81f8316d9a04e35ab84bda5a4a1a26c8e19327d9
dancingKimDH <kimdonghyun0701@gmail.com>
2023-11-08 22:03:05 +0900

    postlist

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -1,45 +1,83 @@
 import { Link } from "react-router-dom";
-import { useState } from "react";
+import { useContext, useEffect, useState } from "react";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "firebaseApp";
+import AuthContext from "context/AuthContext";
 
 interface PostListProps {
-    hasNavigation ?: boolean;
+    hasNavigation?: boolean;
 }
 
 type TapType = "all" | "my"
 
-export default function PostsList({ hasNavigation = true }) {
-    
-    const [activeTab, setActiveTab] = useState<TapType>("all")
-    
+interface PostProps {
+    id: string;
+    title: string;
+    email: string;
+    summary: string;
+    content: string;
+    createdAt: string
+}
+
+export default function PostsList({ hasNavigation = true }: PostListProps) {
+
+    const [activeTab, setActiveTab] = useState<TapType>("all");
+
+    const [posts, setPosts] = useState<PostProps[]>([]);
+
+    const { user } = useContext(AuthContext);
+
+    const getPosts = async () => {
+        const datas = await getDocs(collection(db, "posts"));
+        datas?.forEach((doc) => {
+            const dataObj = { ...doc.data(), id: doc.id };
+            setPosts((prev) => [...prev, dataObj as PostProps]);
+            console.log(dataObj);
+        })
+    }
+
+    useEffect(() => {
+        getPosts();
+    }, [activeTab])
+
+
     return (
         <>
             {hasNavigation && (
                 <div className="post__navigation">
                     <div role="presentation" onClick={() => setActiveTab("all")}
-                    className={activeTab === "all" ? "post__navigation--active" : ""}>전체 글</div>
+                        className={activeTab === "all" ? "post__navigation--active" : ""}>전체 글</div>
                     <div role="presentation" onClick={() => setActiveTab("my")}
-                    className={activeTab === "my" ? "post__navigation--active" : ""} >나의 글</div>
+                        className={activeTab === "my" ? "post__navigation--active" : ""} >나의 글</div>
+
                 </div>
             )}
 
             <div className="post__list">
-                {[...Array(10)].map((e, index) => (
-                    <div key={index} className="post__box">
-                        <Link to={`/posts/${index}`}>
+                {posts?.length > 0 ? posts?.map((post, index) => (
+                    <div key={post?.id} className="post__box">
+                        <Link to={`/posts/${post?.id}`}>
                             <div className="post_profile_box">
                                 <div className="post_profile" />
-                                <div className="post_author_name"> DH</div>
-                                <div className="post_date"> 2023 </div>
+                                <div className="post_author_name">{post?.email}</div>
+                                <div className="post_date"> {post?.createdAt} </div>
                             </div>
-                            <div className="post_title"> 게시글 {index}</div>
-                            <div className="post_text"> hello world! </div>
+                            <div className="post_title"> {post?.title} </div>
+                            <div className="post_text"> {post?.content} </div>
+                        </Link>
+
+
+                        {post?.email === user?.email && (
                             <div className="post__utils-box">
                                 <div className="post__delete">삭제</div>
-                                <div className="post__edit">수정</div>
+                                <Link to={`/posts/edit/${post?.id}`}><div className="post__edit">수정</div></Link>
                             </div>
-                        </Link>
+                        )}
                     </div>
-                ))}
+                )) :
+                    <div className="c">
+                        게시글이 없습니다
+                    </div>}
             </div>
         </>
 
